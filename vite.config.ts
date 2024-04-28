3ef635c7ed2c6e0e74b1b01b4471547f133c29d2
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-04-28 15:51:40 +0000

    📦 Add React and Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), viteSingleFile()],
+  server: {
+    port: 8080,
+    host: "0.0.0.0",
+  },
+});
